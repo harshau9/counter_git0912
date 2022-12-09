0af34f5aabd5bf33e9c8d4c95338aa62b5117e51
Harsha Umesh <harshau129@gmail.com>
2022-12-09 06:08:06 +0530

    cnt

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+  
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const handleClick1 = () => {
+    setCounter(counter + 1)
+  }
+
+  const handleClick2 = () => {
+    setCounter(counter - 1)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '300%',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: '-15%',
+    }}>
+      Counter App
+      <div style={{
+        fontSize: '120%',
+        position: 'relative',
+        top: '10vh',
+      }}>
+        {counter}
+      </div>
+      <div className="buttons">
+        <button style={{
+          fontSize: '60%',
+          position: 'relative',
+          top: '20vh',
+          marginRight: '5px',
+          backgroundColor: 'green',
+          borderRadius: '8%',
+          color: 'white',
+        }}
+          onClick={handleClick1}>Increment</button>
+        <button style={{
+          fontSize: '60%',
+          position: 'relative',
+          top: '20vh',
+          marginLeft: '5px',
+          backgroundColor: 'red',
+          borderRadius: '8%',
+          color: 'white',
+        }}
+          onClick={handleClick2}>Decrement</button>
+      </div>
     </div>
-  );
+  )
 }
+  
+export default App
 
-export default App;
